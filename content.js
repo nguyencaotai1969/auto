c5daad642be98abb90e46ad565df799210ae8c27
nguyencaotai1969 <nguyencaotai1969>
2021-11-23 10:14:24 +0700

    auto_view_page

--- content.js
+++ content.js
@@ -162,6 +162,7 @@ function checkdomain(){
 			}
 
 	        chrome.storage.local.get(['url_random'], function(result) {
+	        	sessionStorage.setItem("count_loadPage",0);
 	        	let locationDomain = result.length !== undefined ? result.url_random : "https://"+domain.host1;
 	          	window.location.href = locationDomain;
 	          	return;
